a6b82af2c4882729bbdc40f10665f10f6923e449
babels <babels134@gmail.com>
2019-08-20 23:38:13 -0400

    day1

---
+++ lab1/HelloWorld.java
@@ -0,0 +1,12 @@
+package LABPractice;				// blue print, pogram name
+
+public class HelloWorld {			// public > exports to other programs/classes
+
+  // main entry point to console application
+  public static void main(String[] args) {
+
+     System.out.println("My first program!");	// System > internal obkect
+
+  }
+
+}
